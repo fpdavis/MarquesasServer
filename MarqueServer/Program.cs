879587c0c807475ec659a81d36846a3fb5ef58c0
pdavis <pdavis@pobox.com>
2017-05-18 23:48:47 -0500

    First Commit

---
+++ MarqueServer/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bend.Util;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Configuration;
+
+
+namespace MarqueServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // WMIC path win32_process get Commandline | grep EnableMarqueServer
+            // "D:\Emulators\RocketLauncher\RocketLauncher.exe" -EnableMarqueServer -s "Atari 7800" -r "D:\Emulators\Roms\Atari 7800\3D Asteroids (1987) (Atari) (Prototype).a78"
+            
+            XMLSettings.LoadPlatformXML();
+
+            int iPort;
+
+            if (string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["Port"]) || !int.TryParse(ConfigurationManager.AppSettings["Port"], out iPort))
+            {
+                Console.WriteLine("Setting default Port to 8080.");
+                iPort = 8080;
+            }
+
+            Console.WriteLine("Available IP Addresses");
+            foreach (var i in System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces())
+                foreach (var ua in i.GetIPProperties().UnicastAddresses)
+                {
+                    if (ua.Address.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        Console.WriteLine("   " + ua.Address);
+                    }                    
+                }
+
+            Console.WriteLine();
+
+            if (! string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["IPAddress"]))
+            {
+                Console.WriteLine("Using IP Address bindings from App.config...");
+
+                var saConfigIPAddresses = ConfigurationManager.AppSettings["IPAddress"].Split(',');
+                IPAddress oIPAddress;
+
+                foreach (string sConfigIPAddress in saConfigIPAddresses)
+                {
+                    if (IPAddress.TryParse(sConfigIPAddress, out oIPAddress))
+                    {
+                        Console.WriteLine("   Binding to " + oIPAddress.ToString() + " on port " + iPort);
+                        HttpServer httpServer = new MyHttpServer(oIPAddress, iPort);
+                        Thread thread = new Thread(new ThreadStart(httpServer.listen));
+                        thread.Start();
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Binding to all available IP Addresses...");
+
+                foreach (var i in System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces())
+                    foreach (var oIPAddress in i.GetIPProperties().UnicastAddresses)
+                    {
+                        if (oIPAddress.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            Console.WriteLine("   Binding to " + oIPAddress.Address.ToString() + " on port " + iPort);
+
+                            try
+                            {
+                                HttpServer httpServer = new MyHttpServer(oIPAddress.Address, iPort);
+                                Thread thread = new Thread(new ThreadStart(httpServer.listen));
+                                thread.Start();
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Could not bind to " + oIPAddress.Address.ToString());
+                            }
+                        }
+                    }
+            }
+
+            ProcessMonitor oProcessMonitor = new ProcessMonitor();
+            oProcessMonitor.WaitForProcess();
+        }
+
+
+    }
+}
---
+++ MarqueServer/XMLSettings.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+using System.Xml.Linq;
+
+namespace MarqueServer
+{
+    class XMLSettings
+    {
+        public static string[] RomPaths { get; private set; }
+        public static string[] Platforms { get; private set; }
+
+        /// <summary>
+        /// The platform settings document.
+        /// </summary>
+        private static XDocument PlatformList { get; set; }
+        public static void LoadPlatformXML()
+        {
+            string sPlatformXMLPath = ConfigurationManager.AppSettings["PlatformXMLPath"];
+            
+            PlatformList = XDocument.Load(sPlatformXMLPath + ".xml");
+            Platforms = PlatformList.Root.Descendants("Name").Select(x => x.Value).ToArray();
+            RomPaths = new string[] { };
+
+            string[] files = Directory.GetFiles(sPlatformXMLPath).Where(p => p.EndsWith(".xml")).ToArray();
+            foreach (var path in files)
+            {
+                XDocument xDoc = XDocument.Load(path);
+
+                // read Xml file
+                RomPaths = RomPaths.Concat( xDoc.Root.Descendants("ApplicationPath").Select(x => x.Value.Replace("..\\", "")).ToArray() ).ToArray();
+            }
+
+        }
+
+        /// <summary>
+        /// Gets the Folder Path for the given Platform and Media Type.
+        /// 
+        /// Users can override the default image paths for platforms. This information is stored in the Data/Platforms.xml file in PlatfromFolder nodes.
+        /// </summary>
+        public static string GetPlatformFolder(string platform, string mediaType)
+        {
+            XElement platformFolder = (from SettingsXml in PlatformList.Descendants("PlatformFolder")
+                                       where SettingsXml.Element("Platform").Value == platform
+                                          && SettingsXml.Element("MediaType").Value == mediaType
+                                       select SettingsXml).FirstOrDefault();
+
+            return platformFolder != null ? platformFolder.Element("FolderPath").Value : "";
+        }
+    }
+}
